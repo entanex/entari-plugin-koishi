ed948559fe5e6b853d01ef1d416ed7cf8571efac
KomoriDev <mute231010@gmail.com>
2026-07-20 11:39:51 +0800

    :sparkles: support file configuartion

--- src/entari_plugin_koishi/config.py
+++ src/entari_plugin_koishi/config.py
@@ -1,4 +1,10 @@
-from arclet.entari import BasicConfModel
+from pathlib import Path
+
+from arclet.entari import BasicConfModel, plugin_config
 
 
-class Config(BasicConfModel): ...
+class Config(BasicConfModel):
+    path: str | Path = "."
+
+
+config = plugin_config(Config)
--- src/entari_plugin_koishi/service.py
+++ src/entari_plugin_koishi/service.py
@@ -1,10 +1,14 @@
 import asyncio
+from pathlib import Path
 
 from arclet.entari import add_service
 from javascript import connection
 from koishi import Context
 from launart import Launart, Service
 from launart.status import Phase
+
+from .config import config
+from .log import logger
 
 
 class KoishiService(Service):
@@ -21,35 +25,37 @@ class KoishiService(Service):
     def __init__(self):
         super().__init__()
         self.ctx = Context({})
+
+        self.config_path = Path(config.path).expanduser().resolve() / "koishi.yml"
+        self._available = self.config_path.is_file()
+        if not self._available:
+            logger.error(f"Koishi config file not found: {self.config_path}")
+            return
+
         self.ctx.requires(
             "@koishijs/plugin-http",
             "@koishijs/plugin-server",
             "@koishijs/plugin-server-satori",
             "koishi-plugin-adapter-onebot",
-            config={
-                "@koishijs/plugin-server": {"port": 5140},
-                "@koishijs/plugin-server-satori": {"path": "/satori"},
-                "koishi-plugin-adapter-onebot": {
-                    "selfId": "3655509951",
-                    "path": "/onebot/v11/ws",
-                },
-            },
+            config=self.config_path,
         )
 
     async def launch(self, manager: Launart):
+        koishi_task = None
         async with self.stage("preparing"):
-            koishi_task = asyncio.create_task(self.ctx._main())
+            if self._available:
+                koishi_task = asyncio.create_task(self.ctx._main())
         async with self.stage("blocking"):
             await manager.status.wait_for_sigexit()
         async with self.stage("cleanup"):
             if connection.is_alive():
                 connection.stop()
 
-            if koishi_task and not koishi_task.done():
+            if koishi_task is not None and not koishi_task.done():
                 koishi_task.cancel()
 
             try:
-                if koishi_task:
+                if koishi_task is not None:
                     await koishi_task
             except asyncio.CancelledError:
                 pass
